55b37337be6bb98d8777e2c6909ee9d79681691d
hopelevin <hopealevin@gmail.com>
2018-08-08 15:46:10 +0000

    automatically backed up by learn

---
+++ dancing.rb
@@ -0,0 +1,19 @@
+def shake_it_out
+  puts "cause the player's gonna"
+  play_count = 0
+  while play_count < 6
+    puts "play"
+    play_count += 1
+  end
+
+  puts "and the hater's gonna"
+  hate_count = 0
+  while hate_count < 6
+    puts "hate"
+    hate_count += 1
+  end
+
+  puts "Baby, I'm just gonna..."
+  end
+
+shake_it_out
